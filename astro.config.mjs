a328d107991bfb28e0a212dcd4bd8cff1889f42a
tombeckett <tpbeckett@gmail.com>
2023-04-18 16:10:24 +0100

    Author fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import alpinejs from "@astrojs/alpinejs";
+import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind()]
+  integrations: [alpine(), tailwind()],
 });
